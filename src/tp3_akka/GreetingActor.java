2c5a6e360b0be849d8e9e7c169b517e7ec34df10
nash403 <honore.nintunze@gmail.com>
2016-03-18 11:10:35 +0100

    Q2-Q3

--- src/tp3_akka/GreetingActor.java
+++ src/tp3_akka/GreetingActor.java
@@ -1,13 +1,29 @@
 package tp3_akka;
 
+import java.util.List;
+
+import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 
 public class GreetingActor extends UntypedActor {
 
+	public List<ActorRef> childs;
+	public String name;
+
+	public GreetingActor(String name, List<ActorRef> system) {
+		this.childs = system;
+		this.name = name;
+	}
+
 	@Override
 	public void onReceive(Object message) throws Exception {
 		if (message instanceof Greeting) {
-			System.out.println("Greeting : " + message);
+			System.out.println(name + " Recois les greeting de : " + message);
+			if (childs != null) {
+				for (ActorRef a : childs) {
+					a.tell(new Greeting(name), ActorRef.noSender());
+				}
+			}
 		} else if (message instanceof String) {
 			System.out.println("Message : " + message);
 		} else {
--- src/tp3_akka/Main.java
+++ src/tp3_akka/Main.java
@@ -1,5 +1,8 @@
 package tp3_akka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
@@ -11,17 +14,62 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		ActorSystem system = ActorSystem.create("MySystem");
-		ActorRef greeter1, greeter2;
-		greeter1 = system
-				.actorOf(Props.create(GreetingActor.class), "greeter1");
-		greeter2 = system
-				.actorOf(Props.create(GreetingActor.class), "greeter2");
+		ActorRef greeter1, greeter2, greeter3, greeter4, greeter5, greeter6;
+
+		List<ActorRef> childsActor1 = new ArrayList<ActorRef>();
+		List<ActorRef> childsActor2 = new ArrayList<ActorRef>();
+		List<ActorRef> childsActor3 = null;
+		List<ActorRef> childsActor4 = null;
+		List<ActorRef> childsActor5 = new ArrayList<ActorRef>();
+		List<ActorRef> childsActor6 = null;
+
+		greeter6 = system.actorOf(
+				Props.create(GreetingActor.class, "Acteur 6", childsActor6),
+				"greeter6");
+
+		childsActor5.add(greeter6);
+		greeter5 = system.actorOf(
+				Props.create(GreetingActor.class, "Acteur 5", childsActor5),
+				"greeter5");
+		greeter4 = system.actorOf(
+				Props.create(GreetingActor.class, "Acteur 4", childsActor4),
+				"greeter4");
+		greeter3 = system.actorOf(
+				Props.create(GreetingActor.class, "Acteur 3", childsActor3),
+				"greeter3");
+
+		childsActor2.add(greeter3);
+		childsActor2.add(greeter4);
+		greeter2 = system.actorOf(
+				Props.create(GreetingActor.class, "Acteur 2", childsActor2),
+				"greeter2");
+
+		childsActor1.add(greeter2);
+		childsActor1.add(greeter5);
+		greeter1 = system.actorOf(
+				Props.create(GreetingActor.class, "Acteur 1", childsActor1),
+				"greeter1");
 
 		// Envois de messages
-		greeter1.tell(new Greeting("Charlie Parker"), ActorRef.noSender());
-		greeter2.tell(new Greeting("Bobby Marley"), ActorRef.noSender());
+		System.out.println("Départ a partir de l'acteur 1 :");
+		greeter1.tell(new Greeting("Main"), ActorRef.noSender());
 
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("\nDépart a partir de l'acteur 2 :");
+		greeter2.tell(new Greeting("Main"), ActorRef.noSender());
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		system.shutdown(); // arrêt du système (fin des acteurs)
 	}
-
 }
